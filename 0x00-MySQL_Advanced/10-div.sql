0edb6aff5968253eaedce6cb014b52f02fc49b88
Gideon-Gyebi <125477741+Gideon-Gyebi@users.noreply.github.com>
2024-01-19 00:20:53 +0000

    Fixing bug (10-div.sql)

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,19 +1,12 @@
 -- This creates a function SafeDiv that divides (and returns) 
 -- the first by the second number or returns 0 if the second
 --number is equal to 0;
-DELIMITER $$ ;
-CREATE FUNCTION SafeDiv(
-	a INT,
-	b INT
-)
+DELIMITER $$
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
-DETERMINISTIC
 BEGIN
-	DECLARE result FLOAT;
-	IF b = 0 THEN
-		RETURN 0;
-        END IF;
-        SET result = (a * 1.0) / b;
-        RETURN result;
-END;$$
+    RETURN (IF (b = 0, 0, a / b));
+END
+$$
 DELIMITER ;
