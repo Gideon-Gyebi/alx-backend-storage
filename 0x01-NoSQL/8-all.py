1e0953aa60eeaea727dd1a5d1fd1970ea2c6ed65
Gideon-Gyebi <125477741+Gideon-Gyebi@users.noreply.github.com>
2024-01-24 01:08:12 +0000

    Task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Listing all documents in a given collection"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """
+    function to list all doc in a collection
+    """
+    if not mongo_collection:
+        return []
+    documents = mongo_collection.find()
+    return [post for post in documents]
